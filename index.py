d9c886c0b2379cc5a01ca3b89e7916d61b77f527
Harrison Ledoux <harrison.ledoux@gmail.com>
2015-02-08 13:58:36 +0100

    +algo +log initializer

--- index.py
+++ index.py
@@ -1,18 +1,10 @@
 import os
 import slots_spots_algo
 import json, ast
-import logging
 import socket 
-from logging.handlers import SysLogHandler
+import utils.log_initializer
 
 from flask import Flask, request, render_template, jsonify
-
-class ContextFilter(logging.Filter):
-	hostname = socket.gethostname()
-	
-	def filter(self, record):
-		record.hostname = ContextFilter.hostname
-		return True
 
 app = Flask(__name__)
 
@@ -27,40 +19,30 @@ def simulator():
 @app.route('/simulator/execute', methods=['GET','POST'])
 def simulator_execute():
 
-	res = {}
+    res = {}
 
-	if request.method == 'POST':
-		json_data_string = json.dumps(request.json, indent=4)
-		json_data_dict = ast.literal_eval(json_data_string)
+    if request.method == 'POST':
+        json_data_string = json.dumps(request.json, indent=4)
+        json_data_dict = ast.literal_eval(json_data_string)
 
-		slot_duration = json_data_dict["slot_duration"]
-		data_spots = []
-		for spot in json_data_dict["data_spots"]:
-			data_spots.append((spot['id'], spot['duration'], spot['value']))
-			app.logger.warning(data_spots);
+        slot_duration = int(json_data_dict["slot_duration"])
+        data_spots = []
+        for spot in json_data_dict["data_spots"]:
+            data_spots.append((spot['id'], spot['duration'], spot['value']))
+            
+        max_benef, sub_spots = slots_spots_algo.compute_max_benef(data_spots, slot_duration)
 
-		max_benef, sub_spots = slots_spots_algo.compute_max_benef(data_spots, slot_duration)
+        sub_spots_json = []
+        for spot in sub_spots:
+            sub_spots_json.append({'id' : spot[0], 'duration' : spot[1], 'value' : spot[2]})
 
-		sub_spots_json = []
-		for spot in sub_spots:
-			sub_spots_json.append({'id' : spot[0], 'duration' : spot[1], 'value' : spot[2]})
-
-		res = {'max_benef': max_benef, 'sub_spots': sub_spots_json}
+        res = {'max_benef': max_benef, 'sub_spots': sub_spots_json}
     
-	return jsonify(res)
+    return jsonify(res)
 
 if __name__ == '__main__':
-	f = ContextFilter()
-	app.logger.addFilter(f)
+    utils.log_initializer.initialize_logger(app.logger, 'logs')
 
-	syslog = SysLogHandler(address=('logs2.papertrailapp.com', 30280))
-	formatter = logging.Formatter('%(asctime)s %(hostname)s slots_spots_local %(message)s', datefmt='%Y-%m-%dT%H:%M:%S')
-
-   	syslog.setFormatter(formatter)
-	app.logger.addHandler(syslog)
-
-	app.logger.info("This is a message")
-
-   	port = int(os.environ.get("PORT", 5000))
-   	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
--- slots_spots_algo.py
+++ slots_spots_algo.py
@@ -1,18 +1,43 @@
 # INPUT spots :
-# 	list of spots with inner data (spot_id, spot_duration, spot_value)
+#   list of spots with inner data (spot_id, spot_duration, spot_value)
 # INPUT slot_duration :
-#	the duration of the slot
+#   the duration of the slot
 # OUTPUT sub_spots :
-#	the selected plots in order to have the maximum benefit
+#   the selected plots in order to have the maximum benefit
 # OUTPUT max_benef :
-#	the value of the maximum benefit
+#   the value of the maximum benefit
 #
 # Computes the maximum profit from the spots set
 def compute_max_benef(spots, slot_duration):
+    N = len(spots)
+    M = computeM(spots, slot_duration)
 
-	# TODO : implement compute_best_gain
+    sub_spots = getBestSet(N, slot_duration, M, spots)
+    max_benef = M[N][slot_duration];
 
-	sub_spots = [(9,40,45), (6,10,15), (3,10,15), (1,20,25), (0,20,25)]
-	max_benef = 125;
+    return max_benef, sub_spots
 
-	return max_benef, sub_spots
+def getBestSet(k, t, M, spots):
+    if k==0: 
+        return []
+    elif M[k][t] ==  M[k-1][t] :
+        return getBestSet(k-1,t,M,spots) 
+    else :
+        id, d, v = spots[k-1]
+        return getBestSet(k-1,t-d,M,spots) + [(k-1,d,v)]
+
+def computeM(spots, T):
+    N = len(spots)
+    M = [[0 for t in range(T+1)] for k in range(N+1)]
+
+    # BASE
+    for t in range(T+1): M[0][t] = 0
+    
+    for k in range(1, N+1):
+        for t in range(T+1):
+            M[k][t] = M[k - 1][t]
+            if t - spots[k-1][1] >= 0:
+                if M[k - 1][t - spots[k-1][1]] + spots[k-1][2] > M[k][t]:
+                    M[k][t] = M[k - 1][t - spots[k-1][1]] + spots[k-1][2]
+
+    return M
